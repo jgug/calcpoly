64ede1a4e189ad7c8780a5afffd61cfc13cf8540
jgug <jgugn1@gmail.com>
2015-06-18 22:40:05 +0300

    Fixed issue: calculate points every time activity recreated (i.e. orientation changed)

--- app/src/main/java/com/vshkl/calcpoly/views/PlotActivity.java
+++ app/src/main/java/com/vshkl/calcpoly/views/PlotActivity.java
@@ -25,6 +25,12 @@ public class PlotActivity extends AppCompatActivity {
 
     private final static int LINE_THICKNESS = 8;
     private final static String EXTRA_NAME = "coefficients";
+    private final static String BUNDLE_ARRAY = "bundle_array";
+
+    private double step;
+    private int size;
+
+    double[] points;
 
     @InjectView(R.id.graph) GraphView graph;
 
@@ -37,8 +43,32 @@ public class PlotActivity extends AppCompatActivity {
         Bundle bundle = this.getIntent().getExtras();
         double[] coefficients = bundle.getDoubleArray(EXTRA_NAME);
 
-        CalculatePoints calculatePoints = new CalculatePoints();
-        calculatePoints.execute(coefficients);
+        SharedPreferences preferences =
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        int max = Integer.parseInt(preferences.getString(
+                getString(R.string.pref_max_key), getString(R.string.pref_max_default)));
+        step = Double.parseDouble(preferences.getString(
+                getString(R.string.pref_step_key), getString(R.string.pref_step_default)));
+        size = (int)(max/step);
+
+        if (savedInstanceState == null) {
+            points = new double[size+1];
+            CalculatePoints calculatePoints = new CalculatePoints();
+            calculatePoints.execute(coefficients);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putDoubleArray(BUNDLE_ARRAY, points);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        points = savedInstanceState.getDoubleArray(BUNDLE_ARRAY);
+        plot(points, size, step);
     }
 
     /**
@@ -56,65 +86,59 @@ public class PlotActivity extends AppCompatActivity {
         return array;
     }
 
+    public void plot(double[] points, int size, final double step) {
+        DataPoint[] dataPoints = new DataPoint[size+1];
+        for (int i = 0; i <= size; i++) {
+            dataPoints[i] = new DataPoint(i, points[i]);
+        }
+
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(dataPoints);
+
+        series.setColor(getResources().getColor(R.color.accent_material_light));
+        series.setThickness(LINE_THICKNESS);
+        series.setOnDataPointTapListener(new OnDataPointTapListener() {
+            @Override
+            public void onTap(Series series, DataPointInterface dataPointInterface) {
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.append("x = ").append((int) (dataPointInterface.getX() * step))
+                        .append("  P(x) = ").append((int) dataPointInterface.getY());
+
+                Toast.makeText(getApplicationContext(),
+                        stringBuilder.toString(), Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
+
+        graph.getGridLabelRenderer()
+                .setGridColor(getResources().getColor(R.color.primary_material_dark));
+        graph.getGridLabelRenderer().setHorizontalLabelsVisible(false);
+        graph.getGridLabelRenderer().setVerticalLabelsVisible(false);
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setMaxX(size);
+        graph.getViewport().setMaxY(series.getHighestValueY());
+        graph.getViewport().setScalable(true);
+        graph.addSeries(series);
+    }
+
     /**
      * Class extending AsyncTask for executing points calculatins not in UI thread,
      * An {@link AsyncTask} subclass
      */
-    private class CalculatePoints extends AsyncTask<double[], Void, LineGraphSeries> {
-        private final SharedPreferences preferences =
-                PreferenceManager.getDefaultSharedPreferences(PlotActivity.this);
-        private final int max = Integer.parseInt(preferences.getString(
-                getString(R.string.pref_max_key), getString(R.string.pref_max_default)));
-        private final double step = Double.parseDouble(preferences.getString(
-                getString(R.string.pref_step_key), getString(R.string.pref_step_default)));
-        private final int size = (int)(max/step);
-
+    private class CalculatePoints extends AsyncTask<double[], Void, double[]> {
         @Override
-        protected LineGraphSeries doInBackground(double[]... params) {
+        protected double[] doInBackground(double[]... params) {
             Callback callback =
                     new Callback(params[0][0], params[0][1], params[0][2], params[0][3]);
             CPolynomialCalculator cpoly = new CPolynomialCalculator();
             cpoly.setCallback(callback);
-
-            double[] points = calculatePoints(cpoly, size, step);
-
-            DataPoint[] dataPoints = new DataPoint[size+1];
-            for (int i = 0; i <= size; i++) {
-                dataPoints[i] = new DataPoint(i, points[i]);
-            }
-
-            LineGraphSeries<DataPoint> series = new LineGraphSeries<>(dataPoints);
-            series.setColor(getResources().getColor(R.color.accent_material_light));
-            series.setThickness(LINE_THICKNESS);
-
-            return series;
+            points = calculatePoints(cpoly, size, step);
+            return points;
         }
 
         @Override
-        protected void onPostExecute(LineGraphSeries series) {
-            series.setOnDataPointTapListener(new OnDataPointTapListener() {
-                @Override
-                public void onTap(Series series, DataPointInterface dataPointInterface) {
-                    StringBuilder stringBuilder = new StringBuilder();
-                    stringBuilder.append("x = ").append((int) (dataPointInterface.getX() * step))
-                            .append("  P(x) = ").append((int) dataPointInterface.getY());
-
-                    Toast.makeText(getApplicationContext(),
-                            stringBuilder.toString(), Toast.LENGTH_SHORT
-                    ).show();
-                }
-            });
-
-            graph.getGridLabelRenderer()
-                    .setGridColor(getResources().getColor(R.color.primary_material_dark));
-            graph.getGridLabelRenderer().setHorizontalLabelsVisible(false);
-            graph.getGridLabelRenderer().setVerticalLabelsVisible(false);
-            graph.getViewport().setXAxisBoundsManual(true);
-            graph.getViewport().setYAxisBoundsManual(true);
-            graph.getViewport().setMaxX(size);
-            graph.getViewport().setMaxY(series.getHighestValueY());
-            graph.getViewport().setScalable(true);
-            graph.addSeries(series);
+        protected void onPostExecute(double[] points) {
+            plot(points, size, step);
         }
     }
 }
